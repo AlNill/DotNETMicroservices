e418832fe04814fdf34e3a174c684a5a57252ec5
alek.nill@yandex.ru <alek.nill@yandex.ru>
2023-11-01 13:38:58 -0700

    Add Profiles and Dtos

---
+++ PlatformService/PlatformService.Web/Profiles/PlatformProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using PlatformService.Web.Dtos;
+using PlatformService.Web.Models;
+
+namespace PlatformService.Web.Profiles;
+
+public class PlatformProfile: Profile
+{
+    public PlatformProfile()
+    {
+        CreateMap<Platform, PlatformReadDto>();
+        CreateMap<PlatformCreateDto, Platform>();
+    }
+}
